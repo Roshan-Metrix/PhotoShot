bb23773d455a225858f3b60f47df340a4a6273af
Roshan Patel <roshanpatel12309@gmail.com>
2025-07-26 16:23:15 +0545

    Commited search features

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,8 +1,15 @@
-import { Search } from "./Search";
-import { Buttons } from "./Buttons";
-import { ImageGallery } from "./ImageGallery";
+import { useState } from 'react';
+import  Search  from "./Search";
+import  Buttons  from "./Buttons";
+import  ImageGallery  from "./ImageGallery";
 
-const Form = () => {
+const Form: React.FC = () => {
+  const [searchTerm,setSearchTerm] = useState<string>('');
+
+  const handleSearchSubmit = (term:string) => {
+    setSearchTerm(term);
+  }
+
   const PhotoName = [
     "Mountain",
     "Birds",
@@ -10,16 +17,16 @@ const Form = () => {
     "Nature"
   ];
   return (
-    <div className="flex justify-center items-center h-screen w-full">
-      <div className="bg-gray-300 w-[80%] h-[80%] rounded-lg shadow-lg text-center">
-        <h1 className="text-3xl font-bold p-4">
+    <div className="flex justify-center items-center h-full w-full py-15 px-5">
+      <div className="bg-gray-300 w-[90%] h-auto rounded-lg shadow-lg text-center p-8">
+        <h1 className="text-4xl font-bold p-8">
           PhotoShot
         </h1>
-        <Search />
+        <Search onSearch={handleSearchSubmit} />
         {PhotoName.map((Name) => (
-          <Buttons Name={Name} key={Name} />
+          <Buttons Name={Name} key={Name} Datas={handleSearchSubmit} />
         ))}
-        <ImageGallery />
+        <ImageGallery searchQuery={searchTerm} />
       </div>
     </div>
   );
