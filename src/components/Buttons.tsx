0fd8274674a3972f010d22fd8ea163eddddff2a3
Roshan Patel <roshanpatel12309@gmail.com>
2025-07-26 16:22:21 +0545

    Added button search functionality

--- src/components/Buttons.tsx
+++ src/components/Buttons.tsx
@@ -1,10 +1,15 @@
-type ButtonsProps = {
-  Name : string
+import React from 'react';
+
+interface Props{
+  Name : string;
+  Datas : (Name: string) => void;
 }
 
-export const Buttons =(props: ButtonsProps) => {
+const Buttons:React.FC<Props> =({ Name, Datas }) => {
   return (
-    <button className="mx-4 my-2 py-1 px-4 bg-neutral-800 text-white rounded hover:bg-blue-600 transition duration-300 cursor-pointer">
-      {props.Name}</button>
+    <button onClick={() => Datas(Name)}className="mx-4 my-5 my-2 py-2 px-4 bg-neutral-800 text-white rounded hover:bg-blue-600 transition duration-300 cursor-pointer">
+      {Name}</button>
   );
 };
+
+export default Buttons;
